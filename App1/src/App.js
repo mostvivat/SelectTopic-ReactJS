57f8f02d53de79434a03d987b94b2928267ac2d0
mostvivat <pokemost@gmail.com>
2023-08-11 14:25:23 +0700

    Edit App.js

--- App1/src/App.js
+++ App1/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import './style.css'
-
+import { Header, Content, Footer } from './func-components'
 function App() {
-  return (
-  <>
-  <h2>DeveloperThai.com</h2>
-  <div className='title'>React & React Native</div>
-  </>)
+//แบบที่ 1 ก ำหนดฟังก์ชันทั้งหมดที่จะแสดงในแบบอำร์เรย์
+return (
+[<Header />, <Content />, <Footer />]
+)
 }
-
-export default App;
+export default App
