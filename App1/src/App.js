cf42ecc5d72f656d12f75f9529ea2979253427e2
mostvivat <pokemost@gmail.com>
2023-08-18 13:52:51 +0700

    Create Banner.js

--- App1/src/App.js
+++ App1/src/App.js
@@ -1,14 +1,6 @@
 import React from 'react'
-import { Header, Content, Footer } from './func-components'
-import Calendar from './class-components'
-function App() {
-return (
-<>
-<Header/>
-<p><center><Calendar/></center></p>
-<Content/>
-<Footer/>
-</>
-)
+import Banner from './banner'
+
+export default function App() {
+    return <Banner/> 
 }
-export default App
---
+++ App1/src/banner.js
@@ -0,0 +1,38 @@
+import React from 'react'
+    function randInt(min, max) {
+    let x = (max - min) + 1
+return min + Math.floor(Math.random() * x)
+}
+export default function Banner() {
+    const r = randInt(1, 3)
+
+let banner = ''
+let url = ''
+
+switch (r) {
+    case 1: 
+        banner = './banners/react.jpg'
+        url = 'https://www.reactjs.org'
+        break
+    case 2:
+        banner = './banners/react-native.jpg'
+        url = 'https://www.reactnative.com'
+        break 
+    case 3:
+        banner = './banners/node.jpg'
+        url = 'https://www.nodejs.org'
+        break
+    default: break
+    }
+    const bannerClick = () => { //Event Handler (Arrow Func)
+        window.open(url)
+        //window.location.href = url
+    }
+    return (
+        <div style={{margin: '20px', textAlign: 'center'}}>
+            <img src={banner} onClick={bannerClick} style={{cursor:'pointer'}} 
+            alt="banner"/>
+        </div>
+    )
+}
+    
