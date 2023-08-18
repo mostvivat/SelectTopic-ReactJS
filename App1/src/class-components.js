e6d7d462c2ee352e68eb0752dee6453f21d18d18
mostvivat <pokemost@gmail.com>
2023-08-18 14:12:35 +0700

    Edit fixclass-componentproblembyusearrowfunc

--- App1/src/class-components.js
+++ App1/src/class-components.js
@@ -1,15 +1,11 @@
-import React,{Component}from 'react'
+import React, {Component} from 'react'
 export default class Button extends Component {
-    constructor() {
-        super()
-        this.onClickButtonOK = this.onClickButtonOK.bind(this)
-    }
     showAlert(msg) {
-      alert(msg)
+        alert(msg)
     }
-    onClickButtonOK() {
-      //Event Handler แบบ Regular Function
-      this.showAlert('Hello')
+    
+    onClickButtonOK = () => { //Event Handler แบบ Arrow Function
+        this.showAlert('Hello') //ใช้ this ได้เลย โดยไม่เกิดปัญหา
     }
     render() {
         return <button onClick={this.onClickButtonOK}>OK</button>
