1f9c2f7552f7a260f736563675374e8bfcf4ece2
mostvivat <pokemost@gmail.com>
2023-09-08 14:11:59 +0700

    Create context_context-content-header.js

--- App1/src/App.js
+++ App1/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import MessageBox from './state-func'
+import { userContext } from './context'
+import Header from './context-header'
+import Content from './context-content'
 
 export default function App() {
-    return <MessageBox/>
+    return (
+        <userContext.Provider value={'Tom Jerry'}>
+            <Header/>
+            <Content/>
+        </userContext.Provider>
+    ) 
 }
---
+++ App1/src/context-content.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { userContext } from './context'
+
+export default function Content(){
+    let user = React.useContext(userContext)
+
+    const contentStyle = {
+        backgroundColor:'#eec',
+        textAlign:'center',
+        margin:10,
+        padding:10
+    }
+    return(
+        <div style={contentStyle}>
+            Hello{user}
+        </div>
+    )
+}
